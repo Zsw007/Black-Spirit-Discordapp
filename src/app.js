1c6b3321e96fe880f2ad682483746ed0bdbccd60
Wayne Zhang <zsw007@gmail.com>
2019-01-22 01:43:05 -0500

    Bot now respond only to mentions or prefix.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 
 const client = new Discord.Client();
 
+// TODO: This is temporary placeholder.
+const settings = {
+  prefix: '!',
+};
+
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
  * received from Discord
@@ -12,16 +17,33 @@ client.on('ready', () => {
 });
 
 // Create an event listener for messages
-client.on('message', message => {
+client.on('message', (message) => {
   // Ignore message from other bots to prevent infinite loop.
   if (message.author.bot) {
     return;
   }
 
-  // If the message is "ping"
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    message.channel.send('pong');
+  // Ignore message if it doesn't mention the bot or use prefix.
+  if (!message.isMentioned(client.user) && !message.content.startsWith(settings.prefix)) {
+    return;
+  }
+
+  // Tokenize the message.
+  let tokens = message.content.trim().split(' ');
+
+  // Discard first token if it is the mention.
+  if (tokens[0].trim() === `<@${client.user.id}>`) {
+    tokens.shift();
+  }
+
+  // Discard prefix if it is used.
+  if (tokens[0].trim().startsWith(settings.prefix)) {
+    tokens[0] = tokens[0].substr(settings.prefix.length);
+  }
+
+  // Respond to pings.
+  if (tokens[0].trim() === 'ping') {
+    message.channel.send(`pong ${Math.round(client.ping)}ms`);
   }
 });
 
